61397e6d9d3546e16f8f1e3d0cf2bfac322f89c0
lionkanzen <lion.kanzen@gmail.com>
2014-07-22 20:10:47 -0600

    changes to new music files

--- gui/common/music.js
+++ gui/common/music.js
@@ -51,11 +51,11 @@ function Music()
 Music.prototype.resetTracks = function()
 {
 	this.tracks = {
-		MENU: ["Hero_Down-lost.ogg"],
+		MENU: ["At_The_Gates_Of_Babylon_.ogg"],
 		PEACE: [],
 		BATTLE: ["Taiko_1.ogg", "Taiko_2.ogg"],
 		VICTORY : ["You_are_Victorious!.ogg"],
-		DEFEAT : ["Dried_Tears.ogg"]
+		DEFEAT : ["Hero_Down-lost.ogg"]
 	};
 };
 
