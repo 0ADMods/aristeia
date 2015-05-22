097ef5bff170bc12dbba179cfa90c20b31a2d16e
Niek ten Brinke <niektb95@gmail.com>
2015-05-22 19:29:54 +0200

    Fix error, bump mod.json version and balance converter

--- gui/common/tooltips_customattack.js
+++ gui/common/tooltips_customattack.js
@@ -3,6 +3,7 @@ function getAttackTypeLabel(type)
 	if (type === "Charge") return translate("Charge Attack:");
 	if (type === "Melee") return translate("Melee Attack:");
 	if (type === "Ranged") return translate("Ranged Attack:");
+	if (type === "Capture") return translate("Capture Attack:");
 	if (type === "Convert") return translate("Convert Attack:");
 
 	warn(sprintf("Internationalization: Unexpected attack type found with code ‘%(attackType)s’. This attack type must be internationalized.", { attackType: type }));
